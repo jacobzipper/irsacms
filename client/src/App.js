a0b00d76a6c3300e636482985b36034747b74ecd
kingenelson <kingenelson@gmail.com>
2020-01-23 05:02:30 -0500

    table test

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import Table from 'react-bootstrap/Table'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Table from "react-bootstrap/Table";
 
-function App() {
-  return (
-    <div className="App">
+class UserRow extends React.Component {
+  state = {
+    imageUrl: 'https://picsum.photo/100'
+  };
+  
+  render() {
+    return (
+      <tr>
+        <td>1</td>
+        <td><img src={this.state.imageUrl}  alt="" /></td>
+        <td>test</td>
+        <td>test</td>
+      </tr>
+    );
+  }
+}
+
+class UserTable extends React.Component {
+  render() {
+    return (
       <Table striped bordered hover>
         <thead>
           <th>#</th>
@@ -15,26 +32,19 @@ function App() {
           <th>data</th>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-          </tr>
+          <UserRow />
+          <UserRow />
+          <UserRow />
         </tbody>
       </Table>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="m-4">
+      <UserTable />
     </div>
   );
 }
