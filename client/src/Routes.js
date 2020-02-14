c2711dfef0bf67dabd94d42bbf0e17cc55b3855c
Rubab Hossain <rhossain6@gatech.edu>
2020-02-14 15:29:28 -0500

    more refactors

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import App from './App';
-import Login from './Login';
+import StudentPage from './StudentPage';
+import LoginPage from './LoginPage';
 
 
 export default function Routes() {
   return (
     <Switch>
       {/* TODO: make / route to a homepage */}
-      <Route exact path='/home' component={App} />
-      <Route exact path='/students' component={App} />
-      <Route exact path='/login' component={Login} />
+      <Route exact path='/home' component={StudentPage} />
+      <Route exact path='/students' component={StudentPage} />
+      <Route exact path='/login' component={LoginPage} />
       {/* <Route component={NotFound} /> */}
     </Switch>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,59 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
 import * as serviceWorker from './serviceWorker';
-import {
-  BrowserRouter as Router,
-  Link
-} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import { Nav, Navbar} from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-
+import Navbar from "./Navbar";
 import Routes from './Routes';
 
-class Page extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          {/* Navigation Bar with Routes */}
-          <Navbar bg="dark" variant="dark" expand="lg">
-            {/* Nav bar properties for responsiveness (i think ?)  */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            {/* Navbar elements that link to places */}
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-
-                <LinkContainer to="/home">
-                  <Nav.Link>IRSA Client Manager</Nav.Link>
-                </LinkContainer>
-
-                <LinkContainer to="/students">
-                  <Nav.Link> Students </Nav.Link>
-                </LinkContainer>
-
-                <LinkContainer to="/login">
-                  <Nav.Link> Login </Nav.Link>
-                </LinkContainer> 
-
-              </Nav>
-            </Navbar.Collapse>
-
-          </Navbar>
-
-          {/* ACTUAL pages that are routed to */}
-          <Routes />
-
-          
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+        {/* This is the Navbar, add more links to places in the app here */}
+        <Navbar /> 
+        
+        {/* Any new components/pages added need to have corresponding route
+            added in this Routes component. */}
+        <Routes />
+    </Router>
+  );
 }
 
-ReactDOM.render(<Page />, document.getElementById('root') );
+
+ReactDOM.render(<App />, document.getElementById('root') );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
